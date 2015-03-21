fa8939e1a36a5ae25f1a30ac296c5f4d783e6255
Santosh Thapa <t.syantos@gmail.com>
2015-03-20 22:42:27 -0400

    searchfilesystem

---
+++ FileSystemSearch/FileSystemSearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace FileSystemSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Give your path
+            GetFilesInDirectory(@"C:\Users\sthapa\Documents\Test");
+            Console.ReadLine();
+        }
+
+        public static ArrayList GetFilesInDirectory(string path)
+        {
+
+            ArrayList fileList = new ArrayList();
+            FindFiles(path, fileList);
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                //Function caling function, recursion happens here. 
+                GetFilesInDirectory(dir);
+            }
+
+            return fileList;
+        }
+
+        private static void FindFiles(string path, ArrayList fileList)
+        {
+            string[] files = Directory.GetFiles(path);
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    fileList.Add(file);
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
